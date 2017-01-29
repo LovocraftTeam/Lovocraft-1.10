1db9dd647a56927d4ff738c908a936761ad4ab4d
Simon <tutadventures@googlemail.com>
2017-01-29 16:34:27 +0100

    FireSource

--- src/main/java/de/alles_minecraft/lovocraft/blocks/BaseBlock.java
+++ src/main/java/de/alles_minecraft/lovocraft/blocks/BaseBlock.java
@@ -4,7 +4,11 @@ import de.alles_minecraft.lovocraft.Lovocraft;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.Entity;
 import net.minecraft.util.EnumBlockRenderType;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 public class BaseBlock extends Block{
 
@@ -16,5 +20,13 @@ public class BaseBlock extends Block{
 	@Override
 	public EnumBlockRenderType getRenderType(IBlockState state) {
 		return EnumBlockRenderType.MODEL;
+	}
+	
+	public boolean isFireSource(World world, BlockPos pos, EnumFacing side) {
+		if (side == EnumFacing.UP) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 }
