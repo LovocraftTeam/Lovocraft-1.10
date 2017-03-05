aa526f903fa53bd3d82b0ac26065792f79164f4e
MrTroble <nifroh56@gmail.com>
2017-03-05 13:33:10 +0100

    Item reg

--- src/main/java/de/alles_minecraft/lovocraft/LItems.java
+++ src/main/java/de/alles_minecraft/lovocraft/LItems.java
@@ -1,8 +1,13 @@
 package de.alles_minecraft.lovocraft;
 
+import io.github.mc_umod.registrys.BlockRegistery;
+
 public class LItems {
 
+	public final BlockRegistery register;
+	
 	public LItems() {
+		this.register = new BlockRegistery();
 		register();
 	}
 
